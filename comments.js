d74659ed1eefc757ec018e9d885f67fc0d4a7407
M.P <81165682+NeuralCodeDev@users.noreply.github.com>
2023-07-17 21:38:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,48 @@
+// Create web server 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const comments = require('./comments');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// GET comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+// GET comment by id
+app.get('/comments/:id', (req, res) => {
+    let id = req.params.id;
+    let comment = comments.find(comment => comment.id === id);
+    res.json(comment);
+});
+
+// POST comment
+app.post('/comments', (req, res) => {
+    let comment = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
+
+// PUT comment
+app.put('/comments/:id', (req, res) => {
+    let id = req.params.id;
+    let comment = req.body;
+    let index = comments.findIndex(comment => comment.id === id);
+    comments[index] = comment;
+    res.json(comment);
+});
+
+// DELETE comment
+app.delete('/comments/:id', (req, res) => {
+    let id = req.params.id;
+    let index = comments.findIndex(comment => comment.id === id);
+    comments.splice(index, 1);
+    res.json(comments);
+});
+
 // Create web server
-// 1. create web server
-// 2. load http module
-// 3. create http server
-// 4. start http server
-// 5. listen for events
-// 6. create request handler
-// 7. return response
-// 8. create request router
-// 9. route requests to request handlers
-
-// 1. create web server
-var http = require('http');
-var url = require('url');
-
-// 3. create http server
-function start(route, handle) {
-	function onRequest(request, response) {
-		var pathname = url.parse(request.url).pathname;
-		console.log('Request for ' + pathname + ' received.');
-
-		// 9. route requests to request handlers
-		route(handle, pathname, response, request);
-	}
-
-	// 4. start http server
-	http.createServer(onRequest).listen(8888);
-	console.log('Server has started.');
-}
-
-// 2. load http module
-exports.start = start;
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
